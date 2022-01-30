fff1dff38959f70e41176bc60eaac934b8105970
trangle1302 <lengochaitrang@gmail.com>
2022-01-30 11:37:45 +0100

    mv codes to helpers

--- helpers/generate_mapping.py
+++ helpers/generate_mapping.py
@@ -16,7 +16,11 @@ row_mapping = {
 
 if __name__ == "__main__":
     meta_ab = pd.read_csv(f"{base_dir}/Experiment_design/meta_ab.csv")
+    meta_ab["platexx"] = [f.split('_')[0] for f in meta_ab.well_id]
+    print(meta_ab[meta_ab.platexx == 8])
     for plate in range(1,9):
+        if plate == 8:
+            continue
         target = json.load(open(f"{base_dir}/covid_data_Scilifelab_portal/plate{plate}.ome.tif_filenames.json"))
         target_df = pd.DataFrame(target)
         target_df["well_id"] = ""
@@ -28,9 +32,13 @@ if __name__ == "__main__":
             plane = str(int(row.files[10:12]))
             row.well_id = "".join([str(plate),"_",r,c])
             row.sample_id = "".join([fov,"_",plane])
+            #print(r,c,fov,plane)
 
         df = target_df.merge(meta_ab[["Antibody","Gene","well_id"]], on="well_id", how="left")
         df["new_names"] = df.Antibody + "_" + df.Gene + "_" + df.sample_id
+        print(plate, df.new_names.value_counts())
+        id_nan = df.new_names.isna()
+        df["new_names"][id_nan] = df.well_id[id_nan] + '_' + df.sample_id
         target["files"] = list(df.new_names.values)
         with open(f"{base_dir}/covid_data_Scilifelab_portal/plate_{plate}.ome.tif_filenames.json", "w") as outfile:
             json.dump(target, outfile)
